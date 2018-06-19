227a9cfcfb82e9fd94cd366f1c42452178467b6d
Nitro_xN <NitronAuthor@gmail.com>
2018-06-20 03:40:50 +0900

    initialize

---
+++ spec/sentence-end-guard-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SentenceEndGuardView from '../lib/sentence-end-guard-view';
+
+describe('SentenceEndGuardView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
